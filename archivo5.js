2870130da91eef051b57f3c66e1c5288a2e2b9f3
MirianBaigorria <mirianbaigo@gmail.com>
2023-10-04 23:14:24 -0300

    agregue un caso más, y cambie algunos colores

--- archivo5.js
+++ archivo5.js
@@ -3,15 +3,17 @@ console.log("\nSeleccione una opción: \n1- Rosa \n2-Azul \n3-Violeta \n4-Amaril
 function decidirColor(opcion){
     switch(opcion){
         case 1: 
-            return "rosa";
+            return "gris";//estaba en rosa, lo puse en gris
         case 2:
             return "azul";
         case 3:
             return "violeta";
         case 4:
-            return "amarillo";
+            return "rosa";//estaba en amarillo, lo puse en rosa
         case 5:
             return "verde";
+        case 6:
+            return "amarillo"; //agregue el caso 6
         default:
             return "La opción elegida es incorrecta."
     }
